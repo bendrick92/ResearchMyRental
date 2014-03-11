83ec7c1b84bce5c27e3249698c5a802a24fea4de
Ben Walters <walters.benj@gmail.com>
2014-03-11 09:01:00 -0500

    Initial commit of files.
    Signed-off-by: Ben Walters <walters.benj@gmail.com>

---
+++ RateMyRental/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using RateMyRental.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RateMyRental.Controllers
+{
+    public class AccountController : BaseController
+    {
+        public ActionResult Index()
+        {
+            Account_IndexViewModel model = new Account_IndexViewModel();
+            return View(model);
+        }
+	}
+}
---
+++ RateMyRental/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using RateMyRental.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RateMyRental.Controllers
+{
+    public class HomeController : BaseController
+    {
+        /// <summary>
+        /// Render Index View for Home
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            Home_IndexViewModel model = new Home_IndexViewModel();
+            return View(model);
+        }
+	}
+}
